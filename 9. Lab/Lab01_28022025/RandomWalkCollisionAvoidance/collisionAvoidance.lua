002744a518d552152e6dfa4191f93336c238dc42
robotic <robotic@ubuntuVm.localdomain>
2025-03-05 14:26:42 +0100

    add collision avoidance

--- 9. Lab/Lab01_28022025/RandomWalkCollisionAvoidance/collisionAvoidance.lua
+++ 9. Lab/Lab01_28022025/RandomWalkCollisionAvoidance/collisionAvoidance.lua
@@ -1,39 +1,45 @@
-
+-- Random walk with collision avoidance
 function init()
-    robot.wheels.set_velocity(0, 0)
+    robot.wheels.set_velocity(5, 5)
 end
 
 function step()
-    light = robot.light
-    left_speed, right_speed = 0, 0
-    max_light = 0
+    local prox = robot.proximity
+    local left_speed, right_speed = 5, 5
     
-    for i = 1, 24 do
-        if light[i].value > max_light then
-            max_light = light[i].value
-            sensor_index = i
+    local obstacle_detected = false
+    local avoidance_turn = 0
+    for i = 1, #prox do
+        if prox[i].value > 0.4 then  
+            obstacle_detected = true
+            avoidance_turn = prox[i].angle  
+            log("Ostacolo rilevato: valore=", prox[i].value, " angolo=", avoidance_turn)
+            break
         end
     end
     
-	angle = light[sensor_index].angle
-	log("Angle: ",math.deg(angle))
-	
-	if math.deg(angle) < -15 then
-		left_speed = 5
-		right_speed = 2
-	elseif math.deg(angle) > 15 then
-		left_speed = 2
-		right_speed = 5
-	else
-		left_speed = 5
-		right_speed = 5
-	end
+    if obstacle_detected then
+       
+        if avoidance_turn > 0 then  
+            left_speed = 5
+            right_speed = -2
+        else 
+            left_speed = -2
+            right_speed = 5
+        end
+    else
+    
+        if math.random() < 0.1 then
+            left_speed = math.random(3, 7)
+            right_speed = math.random(3, 7)
+        end
+    end
     
     robot.wheels.set_velocity(left_speed, right_speed)
 end
 
 function reset()
-    robot.wheels.set_velocity(0, 0)
+    robot.wheels.set_velocity(5, 5)
 end
 
 function destroy()
