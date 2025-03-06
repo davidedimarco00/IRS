26cf40afc3d0c8fabbacf9e6630eb10ffb546cc0
davidedimarco00 <davide.dimarco5@studio.unibo.it>
2025-03-06 23:59:18 +0100

    clean code

--- 9. Lab/Lab01_28022025/Phototaxis/phototaxis.lua
+++ 9. Lab/Lab01_28022025/Phototaxis/phototaxis.lua
@@ -4,31 +4,26 @@ function init()
 end
 
 function step()
-    light = robot.light
+    light_sensors = robot.light
     left_speed, right_speed = 0, 0
     max_light = 0
+    angle_threshold = 15
     
-    for i = 1, 24 do
-        if light[i].value > max_light then
-            max_light = light[i].value
+    for i = 1, #light_sensors do
+        if light_sensors[i].value > max_light then
+            max_light = light_sensors[i].value
             sensor_index = i
         end
     end
-    
-	angle = light[sensor_index].angle
-	log("Angle: ",math.deg(angle))
-	
-	if math.deg(angle) < -15 then
-		left_speed = 5
-		right_speed = 2
-	elseif math.deg(angle) > 15 then
-		left_speed = 2
-		right_speed = 5
+	angle = light_sensors[sensor_index].angle
+
+	if math.deg(angle) < -angle_threshold then
+		left_speed, right_speed = 5, 0
+	elseif math.deg(angle) > angle_threshold then
+		left_speed,  right_speed = 0, 5
 	else
-		left_speed = 5
-		right_speed = 5
+		left_speed, right_speed = 5, 5
 	end
-    
     robot.wheels.set_velocity(left_speed, right_speed)
 end
 
