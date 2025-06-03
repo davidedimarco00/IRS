e1f82d534b07b1fdf7a0b2e97aa4223b6fa9f798
davidedimarco00 <davide.dimarco5@studio.unibo.it>
2025-06-03 22:09:35 +0200

    refactor code

--- Lab04-24032025/controller-ms.lua
+++ Lab04-24032025/controller-ms.lua
@@ -1,48 +1,38 @@
 local vector = require "vector"
 
---parametri
+--parameters
 local L = 0.0
 local MAX_SPEED = 15.0
 local AVOID_THRESHOLD = 0.2
 local LIGHT_THRESHOLD = 0.05
-local LIGHT_INTENSITY_SCALE = 2.0
-local K_OMEGA = 10.0
+local OMEGA = 10.0
 local step_count = 0
 local step_interval = 15
 
 function init()
     L = robot.wheels.axis_length
 end
-
-
--- Potential fields attractive
+--potential field attractive
 function lightAttraction()
     local result = { length = 0.0, angle = 0.0 }
     for i = 1, #robot.light do
         local sensor = robot.light[i]
         local contribution = {
-            length = sensor.value * LIGHT_INTENSITY_SCALE,
+            length = sensor.value,
             angle = sensor.angle
         }
         result = vector.vec2_polar_sum(result, contribution)
     end
     return result
 end
-
-
--- function to reduce speed
 function reduce_speed(v)
-    local sign = v >= 0 and 1 or -1
-    local abs_v = math.abs(v)
-    if abs_v <= MAX_SPEED then
-        return v
+    if math.abs(v) > MAX_SPEED then
+        return MAX_SPEED * (v > 0 and 1 or -1)
     else
-        local reduced = math.sqrt(abs_v)
-        return reduced * sign
+        return v
     end
 end
-
--- potential field repulsive
+--potential field repulsive
 function avoidObstacles()
     local result = { length = 0.0, angle = 0.0 }
     for i = 1, #robot.proximity do
@@ -67,11 +57,8 @@ function avoidObstacles()
     return result
 end
 
-
 function step()
-
-    step_count = step_count + 1 -- step count
-
+    step_count = step_count + 1
     if step_count % step_interval == 0 then
         local light_vec = lightAttraction()
         local avoid_vec = avoidObstacles()
@@ -80,24 +67,17 @@ function step()
             robot.wheels.set_velocity(0, 0)
             return
         end
-
         local v = combined.length
-        local omega = combined.angle * K_OMEGA
-
+        local omega = combined.angle * OMEGA
         local vl = v - (L / 2.0) * omega
         local vr = v + (L / 2.0) * omega
-
         vl = reduce_speed(vl)
         vr = reduce_speed(vr)
         print("reduced: " , math.abs(vl), " ", math.abs(vr))
-
-
         robot.wheels.set_velocity(vl, vr)
     else
         robot.wheels.set_velocity(5,5)
-
     end
-        
 end
 
 function reset()
